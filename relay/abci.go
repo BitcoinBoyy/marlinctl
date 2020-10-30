cb029b146866715419c2aafbc50c6db42bd4268d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-10-31 02:44:12 +0530

    common abci interface and chain mapping

---
+++ relay/abci.go
@@ -0,0 +1,13 @@
+package relay
+
+
+type Abci interface {
+	Create(string) error
+	Destroy() error
+}
+
+var abciMap map[string]Abci = make(map[string]Abci)
+
+func init() {
+	abciMap["eth"] = &EthAbci{}
+}
