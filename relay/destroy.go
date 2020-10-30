56e31880e1cfd151a1f7e770ef484d5299e79169
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-10-31 02:45:00 +0530

    chain-aware relay destroy

--- relay/destroy.go
+++ relay/destroy.go
@@ -10,27 +10,38 @@ import (
 
 
 func DestroyCommand() *cli.Command {
+	var chain string
+
 	return &cli.Command{
 		Name:  "destroy",
 		Usage: "destroy the relay",
-		Flags: []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "chain",
+				Usage:       "--chain \"<CHAIN>\"",
+				Destination: &chain,
+				Required: true,
+			},
+		},
 		Action: func(c *cli.Context) error {
-			out, _ := exec.Command("sudo", "supervisorctl", "status", "relay").Output()
+			program := chain+"_relay"
+
+			out, _ := exec.Command("sudo", "supervisorctl", "status", program).Output()
 			if strings.Contains(string(out), "no such process") {
 				return errors.New("Not found")
 			}
 
-			_, err := exec.Command("sudo", "supervisorctl", "stop", "relay").Output()
+			_, err := exec.Command("sudo", "supervisorctl", "stop", program).Output()
 			if err != nil {
 				return err
 			}
 
-			_, err = exec.Command("sudo", "supervisorctl", "remove", "relay").Output()
+			_, err = exec.Command("sudo", "supervisorctl", "remove", program).Output()
 			if err != nil {
 				return err
 			}
 
-			_, err = exec.Command("sudo", "rm", "/etc/supervisor/conf.d/relay.conf").Output()
+			_, err = exec.Command("sudo", "rm", "/etc/supervisor/conf.d/"+program+".conf").Output()
 			if err != nil {
 				return err
 			}
@@ -38,6 +49,16 @@ func DestroyCommand() *cli.Command {
 			_, err = exec.Command("supervisorctl", "reread").Output()
 			if err != nil {
 				return err
+			}
+
+			// Destroy abci
+			if abci, found := abciMap[chain]; found {
+				err := abci.Destroy()
+				if err != nil {
+					return err
+				}
+			} else {
+				return errors.New("Unrecognized chain")
 			}
 
 			return nil
