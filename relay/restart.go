8e38a9a4ab087434dbb245e2f1983250631599ce
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-11-08 22:39:03 +0530

    Fix: add --chain to restart

--- relay/restart.go
+++ relay/restart.go
@@ -2,20 +2,35 @@ package relay
 
 import (
 	"os/exec"
+	"fmt"
 
 	"github.com/urfave/cli/v2"
 )
 
 func RestartCommand() *cli.Command {
+	var chain string
+
 	return &cli.Command{
 		Name:  "restart",
 		Usage: "restart the relay",
-		Flags: []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "chain",
+				Usage:       "--chain \"<CHAIN>\"",
+				Destination: &chain,
+				Required:    true,
+			},
+		},
 		Action: func(c *cli.Context) error {
-			_, err := exec.Command("sudo", "supervisorctl", "restart", "relay").Output()
+			program := chain + "_relay"
+
+			_, err := exec.Command("sudo", "supervisorctl", "restart", program).Output()
 			if err != nil {
 				return err
 			}
+
+			output, _ := exec.Command("supervisorctl", "status").Output()
+			fmt.Print(string(output))
 
 			return nil
 		},
