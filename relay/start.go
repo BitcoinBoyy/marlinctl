7a55bcae00c154fa81599524079dbe2d20a1d9d6
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-11-08 22:39:20 +0530

    Fix: add --chain to start

--- relay/start.go
+++ relay/start.go
@@ -2,20 +2,35 @@ package relay
 
 import (
 	"os/exec"
+	"fmt"
 
 	"github.com/urfave/cli/v2"
 )
 
 func StartCommand() *cli.Command {
+	var chain string
+
 	return &cli.Command{
 		Name:  "start",
 		Usage: "start the relay",
-		Flags: []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "chain",
+				Usage:       "--chain \"<CHAIN>\"",
+				Destination: &chain,
+				Required:    true,
+			},
+		},
 		Action: func(c *cli.Context) error {
-			_, err := exec.Command("sudo", "supervisorctl", "start", "relay").Output()
+			program := chain + "_relay"
+
+			_, err := exec.Command("sudo", "supervisorctl", "start", program).Output()
 			if err != nil {
 				return err
 			}
+
+			output, _ := exec.Command("supervisorctl", "status").Output()
+			fmt.Print(string(output))
 
 			return nil
 		},
